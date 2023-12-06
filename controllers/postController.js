a6a0576ed2ec6d9b3b1b1b6ae317232e96bf71be
DarshanDixit05 <darshandixit0@gmail.com>
2023-12-06 23:40:19 +0530

    unlike post added

--- controllers/postController.js
+++ controllers/postController.js
@@ -48,23 +48,58 @@ export const addLike = async(req, res) =>{
             })
         }
 
-        const likedUsers = post.likes;
-        if(likedUsers.includes(id)){
+        if(post.likes.some(like => like._id.toString() === id.toString())){
             return res.status(401).json({
                 success:false,
                 message:"You've already liked the post"
             })
         }
-        likedUsers.push(id);
-        post.likes = likedUsers;
+        post.likes.push(id.toString());
         await post.save();
 
-        user.post_liked.push(postId);
+        user.post_liked.push(postId.toString());
         await user.save();
 
         return res.status(200).json({
             success:true,
             message:"Liked the post",
+            data:post
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:"Server error"
+        })
+    }
+}
+
+export const removeLike = async(req,res) =>{
+    const {postId} = req.params;
+    const {id} = req.user;
+
+    try {
+        const post = await Post.findById(postId);
+        const user = await User.findById(id);
+
+        if((post.likes.some(like => like._id.toString() === id.toString())===false) && (user.post_liked.some(like => like._id.toString() === postId.toString())===false)){
+            return res.status(401).json({
+                success:false,
+                message:"You havent liked the post yet"
+            })
+        }
+
+        post.likes = post.likes.filter((ele)=>{
+            return ele.id!==id
+        });
+        await post.save();
+        
+        user.post_liked = user.post_liked.filter((ele)=>ele.id!==postId);
+        await user.save();
+
+        return res.status(200).json({
+            success:true,
+            message:"Unliked the user",
             data:post
         })
     } catch (error) {
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,9 +1,10 @@
 import express from "express"
 const router = express.Router()
-import {createPost, addLike} from "../controllers/postController.js"
+import {createPost, addLike, removeLike} from "../controllers/postController.js"
 import {jwtToken} from "../middlewares/jwtToken.js"
 
 router.route("/createPost").post(jwtToken, createPost);
 router.route("/addLike/:postId").post(jwtToken, addLike);
+router.route("/removeLike/:postId").post(jwtToken, removeLike);
 
 export default router;
